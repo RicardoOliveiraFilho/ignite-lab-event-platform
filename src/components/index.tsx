7c4442e88402eaa99bc33b52a7123b29cb2a6427
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2022-06-24 16:00:15 -0300

    Ignite Lab - Aula 02

---
+++ src/components/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "./Header"
+import { Lesson } from "./Lesson"
+import { Sidebar } from "./Sidebar"
+import { Video } from "./Video"
+
+export {
+  Header,
+  Sidebar,
+  Video,
+  Lesson
+}
