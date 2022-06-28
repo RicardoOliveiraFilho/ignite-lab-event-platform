35836c916d91a14f96b82b47957c736b5b425772
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2022-06-28 17:37:53 -0300

    Ignite Lab - Aula 04

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4rqkmnj1fk401zchi18hsae/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
